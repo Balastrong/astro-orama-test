c7ea40528d4a9644d93cfd3c2f1a077cf1ff5255
Leonardo Montini <lion.48m@gmail.com>
2023-05-05 19:11:53 +0200

    Setup @orama/plugin-astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,21 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import orama from "@orama/plugin-astro";
 
-import sitemap from '@astrojs/sitemap';
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: "https://example.com",
+  integrations: [
+    mdx(),
+    sitemap(),
+    orama({
+      // We can generate more than one DB, with different configurations
+      mydb: {
+        // Required. Only pages matching this path regex will be indexed
+        pathMatcher: /blog.+$/,
+      },
+    }),
+  ],
 });
